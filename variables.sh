e64994715025c320afd72f94e4f1eec12ec2ec14
Nana <nana@MacBook-Pro-de-Nana.local>
2023-04-16 18:05:24 +0200

    v2

--- variables.sh
+++ variables.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 BIRTHDATE="1985-06-19"
-Presents=
-BIRTHDAY= date -d "$date1" +%A
+Presents= 4
+BIRTHDAY= date -d "$BIRTHDATE" +%A
 
 if [ "$BIRTHDATE" == "1985-06-19" ] ; then
     echo "BIRTHDATE is correct, it is $BIRTHDATE"
 else
     echo "BIRTHDATE is incorrect - please retry"
 fi
-if [ $Presents == 10 ] ; then
+if [ $Presents == 4 ] ; then
     echo "I have received $Presents presents"
 else
     echo "Presents is incorrect - please retry"
